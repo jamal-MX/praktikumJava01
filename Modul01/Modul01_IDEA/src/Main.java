37026f684777bc4ceac40ea0051a01f3b693b178
JAMAL-MX <pointleopard007@gmail.com>
2020-04-24 15:53:34 +0800

    Operasi Tipe String

--- Modul01/Modul01_IDEA/src/Main.java
+++ Modul01/Modul01_IDEA/src/Main.java
@@ -1,5 +1,5 @@
-public class Main{
-    public static void main(String [] args){
+public class Main {
+    public static void main(String[] args) {
 
         String namaDepan = "jamal";
         String namaBelakang = "ludin";
@@ -10,5 +10,27 @@ public class Main{
         boolean tampan = true;
 
         System.out.println(namaDepan + namaBelakang);
+
+        char[] uniskaChar = {'U', 'N', 'i', 's', 'k', 'a'};
+        String uniskaString = new String(uniskaChar);
+        System.out.println(uniskaString);
+
+        String namaLengkap = namaDepan + "  " +
+                namaBelakang + "  ";
+
+        System.out.println(namaLengkap.length());
+        System.out.println(namaLengkap.indexOf("NIS"));
+        System.out.println(namaLengkap.substring(5, 12));
+        System.out.println(namaLengkap.replace("yog","Bar"));
+        System.out.println(namaLengkap.toUpperCase());
+        System.out.println(namaLengkap.toLowerCase());
+        System.out.println(namaLengkap.charAt(6));
+
+        String[] namaArray = namaLengkap.split("");
+
+        for (String nama : namaArray) {
+            System.out.println(nama);
+        }
+
     }
 }
