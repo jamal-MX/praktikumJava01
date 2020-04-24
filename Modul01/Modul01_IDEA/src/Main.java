ae123130914220c0fed28e12f9c0e61c76e738ce
JAMAL-MX <pointleopard007@gmail.com>
2020-04-24 15:19:50 +0800

    Project IDEA

---
+++ Modul01/Modul01_IDEA/src/Main.java
@@ -0,0 +1,14 @@
+public class Main{
+    public static void main(String [] args){
+
+        String namaDepan = "jamal";
+        String namaBelakang = "ludin";
+        int usia = 22;
+        int targetTahunKuliah = 4;
+        double ipk = 3.89764512;
+        char nilaiAbjad = 'A';
+        boolean tampan = true;
+
+        System.out.println(namaDepan + namaBelakang);
+    }
+}
