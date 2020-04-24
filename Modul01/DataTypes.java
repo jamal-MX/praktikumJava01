3c6a1d1e6315cea5181493ab38cabd6390afc311
JAMAL-MX <pointleopard007@gmail.com>
2020-04-24 14:35:10 +0800

    Input dengan input Dialog

--- Modul01/DataTypes.java
+++ Modul01/DataTypes.java
@@ -1,5 +1,11 @@
+import javax.swing.JOptionPane;
+import java.util.Scanner;
+
+
 public class DataTypes{
 	public static void main (String [] args ){
+
+	    Scanner scanner = new Scanner (System.in);
 
 	String namaDepan = "jamal";
 	String namaBelakang = "ludin";
@@ -8,6 +14,19 @@ public class DataTypes{
 	double ipk = 3.89764512;
 	char nilaiAbjad = 'A';
 	boolean tampan = true;
+
+	System.out.print("Input nama Depan : ");
+	namaDepan = scanner.nextLine();
+	System.out.print("input Usia : ");
+	usia = scanner.nextInt();
+	System.out.print("Input IPK : ");
+	ipk = scanner.nextDouble();
+	System.out.print("");
+	System.out.print("Input Nilai Abjad : ");
+	nilaiAbjad = scanner.next().charAt(0);
+	System.out.print("Tampan? : ");
+	tampan = scanner.nextBoolean();
+
 	
 
 	System.out.println("Nama depan : " + namaDepan);
@@ -17,5 +36,8 @@ public class DataTypes{
 	System.out.println("Ipk : "+ ipk);
 	System.out.println("Nilai PBO : "+ nilaiAbjad);
 	System.err.println("Tampan :" +tampan);
+
+	JOptionPane.showMessageDialog(null,"Hai," + namaDepan + namaBelakang);
+
 	}
 }
