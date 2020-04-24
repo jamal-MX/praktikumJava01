6d9a0f2387f528e48256ae3c66d929387ed02129
JAMAL-MX <pointleopard007@gmail.com>
2020-04-24 10:23:49 +0800

    deklarasi variable dan tipe data

---
+++ Modul01/DataTypes.java
@@ -0,0 +1,12 @@
+public class DataTypes{
+	public static void main (String [] args ){
+
+	String namaDepan = "jamal";
+	String namaBelakang = "ludin";
+	int usia = 22;
+	int targetKuliah = 4;
+	double ipk = 3.89764512;
+	char nilaiAbjad = 'A';
+	boolean tampan = true;
+	}
+}
