30086fab3eadd54619eb9c9a43c467c79a3b39a3
JAMAL-MX <pointleopard007@gmail.com>
2020-05-07 15:57:14 +0800

    Gui By Text

--- Modul03/src/GuiByText.java
+++ Modul03/src/GuiByText.java
@@ -8,6 +8,14 @@ public class GuiByText extends JFrame {
     JTextField textField = new JTextField();
     JButton button1 = new JButton("Greet");
 
+    JLabel label2 = new JLabel("Panjang");
+    JLabel label3 = new JLabel("Lebar");
+    JLabel label4 = new JLabel("Hasil");
+    JTextField tPanjang = new JTextField();
+    JTextField tLebar = new JTextField();
+    JTextField tHasil = new JTextField();
+    JButton bHitung = new JButton("Hitung Luas");
+
     GuiByText() {
         setSize(640, 480);
         setLocationRelativeTo(null);
@@ -23,10 +31,24 @@ public class GuiByText extends JFrame {
         getContentPane().add(label1);
         getContentPane().add(textField);
         getContentPane().add(button1);
+        getContentPane().add(label2);
+        getContentPane().add(label3);
+        getContentPane().add(label4);
+        getContentPane().add(tPanjang);
+        getContentPane().add(tLebar);
+        getContentPane().add(tHasil);
+        getContentPane().add(bHitung);
 
         label1.setBounds(16, 32, 100, 25);
         textField.setBounds(124, 32, 100, 25);
         button1.setBounds(232, 32, 80, 25);
+        label2.setBounds(16, 80, 100, 25);
+        tPanjang.setBounds(124, 80, 100, 25);
+        label3.setBounds(16, 113, 100, 25);
+        tLebar.setBounds(124, 113, 100, 25);
+        bHitung.setBounds(16, 146, 208, 25);
+        label4.setBounds(16, 179, 100, 25);
+        tHasil.setBounds(124, 179, 100, 25);
 
 
     }
@@ -34,17 +56,28 @@ public class GuiByText extends JFrame {
     public static void main(String[] args) {
         GuiByText guiByText = new GuiByText();
         guiByText.meletakkanKomponen();
+        guiByText.menambahkanListener();
         guiByText.setVisible(true);
     }
 
 
-void menambahkanListener() {
-    button1.addActionListener(new AbstractAction() {
-        @Override
-        public void actionPerformed(ActionEvent actionEvent) {
-            String nama = textField.getText();
-            JOptionPane.showInternalMessageDialog(null, "hai" + null);
-        }
-    });
-}
+    void menambahkanListener() {
+
+        button1.addActionListener(actionEvent -> {
+
+            JOptionPane.showMessageDialog(null,
+                    "Hai");
+        });
+        bHitung.addActionListener(actionEvent -> {
+            int panjang = Integer.parseInt(tPanjang.getText());
+            int lebar = Integer.parseInt(tLebar.getText());
+
+            int hasil = panjang * lebar;
+            String sHasil = String.valueOf(hasil);
+
+
+        });
+
+    }
+
 }
